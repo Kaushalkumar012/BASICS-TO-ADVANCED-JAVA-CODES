d8f6abf1aea1a2a02606036405530157f7d38ab7
Kaushal kumar <146443100+Kaushalkumar012@users.noreply.github.com>
2025-06-01 18:48:12 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,20 @@
+public class Datatype {
+     public static void main(String args[]){
+    int a =10;
+    float f= 1.01f;
+    double l =1.93999;
+   
+    long b= 8837;
+    short d= 24;
+    byte e= 8;
+     System.out.println(a);
+     System.out.println(f);
+     System.out.println(l);
+     System.out.println(b);
+     System.out.println(d);
+     System.out.println(e);
+ }
+}
+
+
+ 
