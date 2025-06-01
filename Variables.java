62edb8b23eaac9d56c89694ea3546ac64490fee4
Kaushal kumar <146443100+Kaushalkumar012@users.noreply.github.com>
2025-06-01 18:06:49 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        String name ="kaushal";
+        System.out.println(a);
+        System.err.println(b);
+        System.out.println(name);
+    }
+    
+}
