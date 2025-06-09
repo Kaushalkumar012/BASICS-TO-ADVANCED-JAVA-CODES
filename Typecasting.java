3cf151b0d2d36737e532b38f00c771e57b57f659
Kaushal kumar <146443100+Kaushalkumar012@users.noreply.github.com>
2025-06-09 09:02:38 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Typecasting {
+    public static void main(String arg []){
+        Scanner sc = new Scanner(System.in);
+        char a = (char)sc.nextShort();
+        System.out.println(a);
+    }
+}
