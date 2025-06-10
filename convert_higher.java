50cb07852c4f783662df85d72fbb3ec07f811b4e
Kaushal kumar <146443100+Kaushalkumar012@users.noreply.github.com>
2025-06-10 12:31:24 +0530

    Add files via upload

---
+++ convert_higher.java
@@ -0,0 +1,10 @@
+public class convert_higher{
+    public static void main(String args[]){
+      long a =60;
+      int b =10;
+      double d =90;
+      double ans = a+b;
+      System.out.println(ans);
+     
+    }
+}
